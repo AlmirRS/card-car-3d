662bb3f2a31368a2067ef3b4e4923f0024117fac
Almir Ramos <almirrdsnegro@gmail.com>
2021-12-13 00:59:59 -0300

    tentando resolver o translateZ, ainda sem solução

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
-//Movement Animation to happen
-const card = document.querySelector(".card");
+// //Movement Animation to happen
+const cartao = document.querySelector(".cartao");
 const container = document.querySelector(".container");
-//Items
-const title = document.querySelector(".title");
-const sneaker = document.querySelector(".sneaker img");
-const purchase = document.querySelector(".purchase");
-const description = document.querySelector(".info h3");
-const sizes = document.querySelector(".sizes");
 
 //Moving Animation Event
-container.addEventListener("mousemove", (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
-  let yAxis = (window.innerHeight / 2 - e.pageY) / 10;
-  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+container.addEventListener('mousemove', (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  cartao.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
-//Animate In
-container.addEventListener("mouseenter", (e) => {
-  card.style.transition = "none";
+
+// //Items
+const titulo = document.querySelector(".titulo");
+const tenis = document.querySelector(".tenis img");
+const comprar = document.querySelector(".comprar button");
+const descricoes = document.querySelector(".descricoes h3");
+const medidas = document.querySelector(".medidas p");
+
+
+// //Animacao entrando
+  container.addEventListener("mouseenter", (e) => {
+  cartao.style.transition = "none";
   //Popout
-  title.style.transform = "translateZ(150px)";
-  sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
-  description.style.transform = "translateZ(125px)";
-  sizes.style.transform = "translateZ(100px)";
-  purchase.style.transform = "translateZ(75px)";
+  titulo.style.transform = "translateZ(150px)";
+  tenis.style.transform = "translateZ(200px) rotateZ(-45deg)";
+  descricoes.style.transform = "translateZ(125px)";
+  medidas.style.transform = "translateZ(100px)";
+  comprar.style.transform = "translateZ(75px)";
 });
-//Animate Out
+//Animação saindo
 container.addEventListener("mouseleave", (e) => {
-  card.style.transition = "all 0.5s ease";
-  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  cartao.style.transition = "all 0.5s ease";
+  cartao.style.transform = `rotateY(0deg) rotateX(0deg)`;
   //Popback
-  title.style.transform = "translateZ(0px)";
-  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
-  description.style.transform = "translateZ(0px)";
-  sizes.style.transform = "translateZ(0px)";
-  purchase.style.transform = "translateZ(0px)";
+  titulo.style.transform = "translateZ(0px)";
+  tenis.style.transform = "translateZ(0px) rotateZ(0deg)";
+  descricoes.style.transform = "translateZ(0px)";
+  medidas.style.transform = "translateZ(0px)";
+  comprar.style.transform = "translateZ(0px)";
 });
